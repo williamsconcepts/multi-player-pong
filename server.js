8735fe3cefcfb081a905346b933b3b6c8362bc70
boo Bae <chidiekwebelam@gmail.com>
2023-03-02 09:40:24 +0100

    Pong Game

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const io = require('socket.io');
+
+const apiServer = require('./api');
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+const sockets = require('./sockets');
+
+const PORT = 3000;
+
+httpServer.listen(PORT);
+console.log(`listening on port ${PORT}...`);
+
+sockets.listen(socketServer);
+
+
+
